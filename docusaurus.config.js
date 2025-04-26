5cc7ae0d4fb1b377fd27c8bd09f90da3ec9b4b37
unitedprogressparty <theuppunitedprogressparty@gmail.com>
2025-04-25 21:38:40 -0700

    Fix: Corrected blog editUrl in Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/unitedprogressparty/firstpub-Platform/tree/main/website/docs/',
+            'https://github.com/unitedprogressparty/firstpub-Platform/tree/main/website/blog/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
